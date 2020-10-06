23eeb54011036bf49ab421b44e836deffc595fef
JoaoOliveira <victorfnguerra@gmail.com>
2020-10-06 12:00:03 -0300

    Pagina de início e  carregamento

--- src/screens/Homepage/Homepage.js
+++ src/screens/Homepage/Homepage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ImageLogo } from './styled-homepage';
+const logo = require('../../img/logo.png');
 
 const Homepage = () => {
   return (
-    <div>Home</div>
+    <div>
+      <ImageLogo src={logo} />
+    </div>
   );
-}
+};
 
 export default Homepage;
---
+++ src/screens/Homepage/styled-homepage.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ImageLogo = styled.img`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -110px;
+  margin-top: -40px;
+  width: 220px;
+  heigth: 80px;
+`;
