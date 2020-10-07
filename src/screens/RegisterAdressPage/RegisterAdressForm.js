9b1bc5a5d82497cf143ce821a7a447fc0b132c35
Daniel Melo <danielfarias380@gmail.com>
2020-10-07 15:14:38 -0300

    Integrate API AddressForm

--- src/screens/RegisterAdressPage/RegisterAdressForm.js
+++ src/screens/RegisterAdressPage/RegisterAdressForm.js
@@ -1,72 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from './styled-register-adress'
 import { Button } from '../../styles/atoms'
 import { Label } from '../../styles/molecules'
-import { goToHomepage } from '../../navigation/Coordinator'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import api from '../../services/api'
+
+const initialState = {
+  street: '',
+  number: '',
+  neighbourhood: '',
+  city: '',
+  state: '',
+  complement: ''
+}
 
 const RegisterAdressForm = () => {
     const history = useHistory()
+    const [form, setForm] = useState(initialState)
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      const token = localStorage.getItem('token')
+
+      api.put('/address', form, {
+        headers: {
+          auth: token
+        }
+      }).then(response => {
+          localStorage.setItem('user', JSON.stringify(response.data.user))
+          localStorage.setItem('token', response.data.token)
+          history.push('/pagina-inicial')
+        })
+        .catch(error => {
+          console.log(error)
+          setForm(initialState)
+        })
+    }
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setForm(state => ({...state, [name]: value}))
+    }
+
     return (
-        <Form>
-        <Label htmlFor="email-login">
+      <Form onSubmit={handleSubmit}>
+        <Label htmlFor="logradouro">
           <span>Logradouro*</span>
           <input
             id="logradouro"
+            name="street"
             type="text"
             placeholder="Rua / Av."
+            value={form.street}
+            onChange={handleChange}
             required
             autoFocus
           />
         </Label>
 
-        <Label htmlFor="password-login">
+        <Label htmlFor="number-home">
           <span>Número*</span>
           <input
             id="number-home"
+            name="number"
             type="number"
             placeholder="Número"
+            value={form.number}
+            onChange={handleChange}
             required
           />
         </Label>
-        <Label htmlFor="password-login">
+        <Label htmlFor="complemento-home">
           <span>Complemento</span>
           <input
             id="complemento-home"
+            name="complement"
             type="text"
+            value={form.complement}
+            onChange={handleChange}
             placeholder="Apto. / Bloco"
-            required
           />
         </Label>
-        <Label htmlFor="password-login">
+        <Label htmlFor="bairro">
           <span>Bairro*</span>
           <input
             id="bairro"
+            name="neighbourhood"
             type="text"
             placeholder="Bairro"
+            value={form.neighbourhood}
+            onChange={handleChange}
             required
           />
         </Label>
-        <Label htmlFor="password-login">
+        <Label htmlFor="cidade">
           <span>Cidade*</span>
           <input
             id="cidade"
+            name="city"
             type="text"
             placeholder="Cidade"
+            value={form.city}
+            onChange={handleChange}
             required
           />
         </Label>
-        <Label htmlFor="password-login">
+        <Label htmlFor="estado">
           <span>Estado*</span>
           <input
             id="estado"
+            name="state"
             type="text"
+            value={form.state}
+            onChange={handleChange}
             placeholder="Estado"
             required
           />
         </Label>
 
-        <Button type="submit" onClick={() => goToHomepage(history)}>Salvar</Button>
+        <Button type="submit">Salvar</Button>
       </Form>
     )
 }
