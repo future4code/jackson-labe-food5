e968a921a7d973343ed493ad8e4d6be24da838ec
Daniel Melo <danielfarias380@gmail.com>
2020-10-07 14:08:39 -0300

    Edit profile page and edit adress page

--- src/screens/ProfilePage/AdressEditPage.js
+++ src/screens/ProfilePage/AdressEditPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components'
 import { InnerScreen } from '../../styles/atoms';
+import RegisterAdressForm from '../RegisterAdressPage/RegisterAdressForm'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const AdressEditPage = () => {
   return (
     <InnerScreen>
-      <div>Editar endereço</div>
+      <Container>
+        <RegisterAdressForm />
+      </Container>
     </InnerScreen>
   );
 }
