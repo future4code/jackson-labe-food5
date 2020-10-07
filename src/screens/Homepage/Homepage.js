a41edfea87fbbd7584b560f2261f5b641cf92193
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-10-07 09:11:24 -0300

    Layout inicial da Homepage.

--- src/screens/Homepage/Homepage.js
+++ src/screens/Homepage/Homepage.js
@@ -1,8 +1,71 @@
 import React from 'react';
+import { 
+  HomepageContainer,
+  SearchIcon,
+  SearchInput,
+  SearchContainer,
+  FilterContainer,
+  FilterSpan,
+  RestaurantCard,
+  RestaurantContainer, 
+  RestaurantImage,
+  UserHandleContainer,
+  RestaurantName, SubInfos, DeliveryTime, Freight 
+} from './styled-homepage'
+import healthy from '../../Img/healthy-food.jpg'
 
 const Homepage = () => {
   return (
-    <div>Home</div>
+    <HomepageContainer>
+      <UserHandleContainer>
+        <SearchContainer>
+          <SearchIcon/>
+          <SearchInput placeholder={"Restaurante"}/>
+        </SearchContainer>
+        <FilterContainer>
+          <FilterSpan>Burger</FilterSpan>
+          <FilterSpan>Asiática</FilterSpan>
+          <FilterSpan>Massas</FilterSpan>
+          <FilterSpan>Saudáveis</FilterSpan>
+          <FilterSpan>Vegano</FilterSpan>
+        </FilterContainer>
+      </UserHandleContainer>
+      <RestaurantContainer>
+        <RestaurantCard>
+          <RestaurantImage src={healthy}/>
+          <RestaurantName>Bullguer Eldorado</RestaurantName>
+          <SubInfos>
+            <DeliveryTime>50 - 60 min</DeliveryTime>
+            <Freight>Frete R$6,00</Freight>
+          </SubInfos>
+        </RestaurantCard>
+        <RestaurantCard>
+          <RestaurantImage src={healthy}/>
+          <RestaurantName>Vinil Butantã</RestaurantName>
+          <SubInfos>
+            <DeliveryTime>50 - 60 min</DeliveryTime>
+            <Freight>Frete R$6,00</Freight>
+          </SubInfos>
+        </RestaurantCard>
+        <RestaurantCard>
+          <RestaurantImage src={healthy}/>
+          <RestaurantName>Juicy Burguer</RestaurantName>
+          <SubInfos>
+            <DeliveryTime>50 - 60 min</DeliveryTime>
+            <Freight>Frete R$6,00</Freight>
+          </SubInfos>
+        </RestaurantCard>
+        <RestaurantCard>
+          <RestaurantImage src={healthy}/>
+        </RestaurantCard>
+        <RestaurantCard>
+          <RestaurantImage src={healthy}/>
+        </RestaurantCard>
+        <RestaurantCard>
+          <RestaurantImage src={healthy}/>
+        </RestaurantCard> 
+      </RestaurantContainer>
+    </HomepageContainer>
   );
 }
 
