f779e7f5b05357cb579cdbe13ffb7472e6e7fea7
Michelle Louzada <hinatah.louz@gmail.com>
2020-10-09 15:14:57 -0300

    incremento na senha pra avisar o usuario das senhas diferentes

--- src/screens/SignUpPage/SignUpForm.js
+++ src/screens/SignUpPage/SignUpForm.js
@@ -45,6 +45,8 @@ const SignUpForm = () => {
       if (isValid) {
         signUp(form, history, setIsLoading)
       }
+    }else {
+     alert('senhas diferentes, por favor verifique sua senha')
     }
     
   }
