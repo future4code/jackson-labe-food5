5f19261696c3856851fc4cb6a1f4766383627aa4
luaanaapreiraa <luanapdsantos@gmail.com>
2020-10-09 11:08:11 -0300

    initial tests

---
+++ src/Tests/RegisterAdress.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react'
+import RegisterAdress from './RegisterAdressForm.js';
+import { ExpansionPanelActions } from '@material-ui/core';
+
+
+describe("Register Adress",()=>{
+    test("Testing Register Adress",()=>{
+        const { getByPlaceholderText, getByText} = render(<RegisterAdressForm/>);
+
+        const input = getByPlaceholderText("Rua / Av.");
+
+        fireEvent.change(input, {target:{value: "Test street"}});
+
+        expect(input).toHaveValue("Test street")
+        
+        const button = getByText("Salvar");
+        
+        fireEvent.click(button);
+
+    })
+})
